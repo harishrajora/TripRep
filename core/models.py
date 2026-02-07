76929af03134d1ab302cdd4bfd1bc10651f24cda
Harish Rajora <harrylitterell@yahoo.in>
2026-02-07 21:10:03 +0530

    Reservations model added.

--- core/models.py
+++ core/models.py
@@ -69,3 +69,19 @@ class Tickets(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Reservations(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=False)
+    reservation_name = models.CharField(max_length=200, null=False, blank=False)
+    reservation_file = models.FileField(upload_to='reservations/')
+    description = models.TextField()
+    date_of_reservation = models.DateField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    reservation_type = models.CharField(max_length=100, null=False, blank=False)
+    booked_through = models.CharField(max_length=100, null=False, blank=False)
+    image_thumbnail = models.ImageField(upload_to='reservation_thumbnails/', null=True, blank=True)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False, default=0.00)
+
+    def __str__(self):
+        return self.reservation_id
